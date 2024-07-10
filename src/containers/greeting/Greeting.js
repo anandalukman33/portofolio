da344702fddc5374799880131a291545a0eb31d7
Ananda Muhamad Lukman <ananda.muhamad.lukman@bni.co.id>
2024-07-10 16:28:26 +0700

    cv

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -41,8 +41,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./CV-Ananda-Muhamad-Lukman.pdf")}
+                    download="CV-Ananda-Muhamad-Lukman.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
